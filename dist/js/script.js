eb2140ee3e7dfb827119af948543042f85967948
Юрий Смолко <yurysmolka@gmail.com>
2021-09-30 12:17:01 +0300

    tabs page

--- dist/js/script.js
+++ dist/js/script.js
@@ -44,4 +44,58 @@ document.addEventListener('DOMContentLoaded', () => {
             placeholder: false,
         });
     }catch(e){}
+
+
+
+    // tabs
+    const tabs__btns = document.querySelectorAll('.tabs__btn'),
+          tabs__content = document.querySelectorAll('.tabs__content');
+
+    tabs__content.forEach(content => {
+        content.classList.add('fadeOut');
+        content.classList.remove('fade');
+        content.style.display = 'none';
+    });
+
+    tabs__content[0].classList.remove('fadeOut');
+    tabs__content[0].classList.add('fade');
+    tabs__content[0].style.display = 'flex';
+
+    tabs__btns.forEach((btn, i )=> {
+        btn.addEventListener('click', e => {
+            e.preventDefault();
+            tabs__btns.forEach( btn => {
+                btn.classList.remove('tabs__btn_active');
+            })
+            if(btn === e.target) {
+                btn.classList.add('tabs__btn_active');
+                tabs__content.forEach(content => {
+                    content.classList.add('fadeOut');
+                    content.classList.remove('fade');
+                    content.style.display = 'none';     
+                });
+            }
+            tabs__content[i].classList.remove('fadeOut');
+            tabs__content[i].classList.add('fade');
+            tabs__content[i].style.display = 'flex';
+        })
+    })
+
+    //tabs selects
+    const models = document.querySelectorAll('#model');
+
+    models.forEach(item => {
+        try{
+            const element = item;
+            const choices = new Choices(element, {
+                searchEnabled: false,
+                shouldSort: false,
+                itemSelectText: '',
+                placeholder: false,
+            });
+        }catch(e){}
+    
+    });
+
+
 });
